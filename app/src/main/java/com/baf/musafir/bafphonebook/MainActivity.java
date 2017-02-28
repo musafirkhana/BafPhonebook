0507a7f3d581d771e0d28d9110732edbf36b16cd
musafir <ali.mbstu.ict@gmail.com>
2017-02-28 20:20:23 +0600

    First Commit

---
+++ app/src/main/java/com/baf/musafir/bafphonebook/MainActivity.java
@@ -0,0 +1,13 @@
+package com.baf.musafir.bafphonebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
