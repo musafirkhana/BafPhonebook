1d5a623a769825bbecc275ce9b4b5627cd149853
musafir <ali.mbstu.ict@gmail.com>
2017-04-21 23:40:55 +0600

    db

--- app/src/main/java/com/baf/musafir/bafphonebook/HomeActivity.java
+++ app/src/main/java/com/baf/musafir/bafphonebook/HomeActivity.java
@@ -6,13 +6,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.baf.musafir.bafphonebook.databse.DataBaseUtility;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends Activity  {
     private Context mContext;
     private DataBaseUtility dataBaseUtility;
+
+    private LinearLayout main_menu_li;
+    private ImageView menu_imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +29,98 @@ public class HomeActivity extends Activity {
         setContentView(R.layout.activity_home);
         mContext = this;
         dataBaseUtility = new DataBaseUtility();
+        initUI();
+    }
+
+    private void initUI() {
+        main_menu_li = (LinearLayout) findViewById(R.id.main_menu_li);
+        menu_imageview=(ImageView)findViewById(R.id.menu_imageview);
     }
 
     public void AIRHQ(View v) {
         dataBaseUtility.getAirHqLodgerData(mContext);
         Intent intent = new Intent(this, ContactListActivity.class);
-        intent.putExtra("header","AIR HQ & ITS LODGER UNITS");
-        startActivity(intent);
-    }
-    public void ZHR(View v) {
-        Intent intent = new Intent(this, ZhrActivity.class);
-        intent.putExtra("header","AIR HQ & ITS LODGER UNITS");
+        intent.putExtra("header", "AIR HQ & ITS LODGER UNITS");
         startActivity(intent);
     }
 
+    public void ZHR(View v) {
+        Intent intent = new Intent(this, ZhrActivity.class);
+        intent.putExtra("header", "AIR HQ & ITS LODGER UNITS");
+        startActivity(intent);
+    }
+    public void MENU(View v) {
+        Toast.makeText(mContext,"dsdsds",Toast.LENGTH_LONG);
+        if (main_menu_li.getVisibility() == View.VISIBLE) {
+            main_menu_li.startAnimation(outToleftAnimation());
+            main_menu_li.setVisibility(View.GONE);
+        } else {
+            main_menu_li.startAnimation(inFromLeftAnimation());
+            main_menu_li.setVisibility(View.VISIBLE);
+            main_menu_li.bringToFront();
+
+        }
+    }
+
+
+    private Animation inFromLeftAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromLeft.setDuration(500);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        return inFromLeft;
+    }
+    private Animation outToleftAnimation() {
+        Animation inFromLeft = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, -1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromLeft.setDuration(500);
+        inFromLeft.setInterpolator(new AccelerateInterpolator());
+        return inFromLeft;
+    }
+    private Animation outToRightAnimation() {
+        Animation outtoRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        outtoRight.setDuration(500);
+        outtoRight.setInterpolator(new AccelerateInterpolator());
+        return outtoRight;
+    }
+
+    private Animation inFromRightAnimation() {
+
+        Animation inFromRight = new TranslateAnimation(
+                Animation.RELATIVE_TO_PARENT, +1.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f,
+                Animation.RELATIVE_TO_PARENT, 0.0f);
+        inFromRight.setDuration(500);
+        inFromRight.setInterpolator(new AccelerateInterpolator());
+        return inFromRight;
+    }
+
+  /*  @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.menu_imageview:
+                Toast.makeText(mContext,"dsdsds",Toast.LENGTH_LONG);
+                if (main_menu_li.getVisibility() == View.VISIBLE) {
+                    main_menu_li.startAnimation(outToRightAnimation());
+                    main_menu_li.setVisibility(View.GONE);
+                } else {
+                    main_menu_li.startAnimation(inFromRightAnimation());
+                    main_menu_li.setVisibility(View.VISIBLE);
+                    main_menu_li.bringToFront();
+
+                }
+                break;
+        }
+    }*/
 }
